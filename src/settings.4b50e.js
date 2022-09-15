5ee9afd5ac44afc7310366fdb4964aaa8a5926a9
george <453617479@qq.com>
2022-09-15 09:47:06 +0800

    upload

---
+++ src/settings.4b50e.js
@@ -0,0 +1,15 @@
+"use strict";
+window._CCSettings = {
+    platform: "web-mobil",
+    groupList: ["default"],
+    collisionMatrix: [[true]],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: ["resources", "main"],
+    subpackages: [],
+    launchScene: "db://assets/scene/main.fire",
+    orientation: "portrait",
+    server: "https://cat-match-static.easygame2021.com/catMatch/sheep_1_2_8/",
+    jsList: [],
+    bundleVers: {internal: "e6604", resources: "8b75c", main: "0df91"}
+};
